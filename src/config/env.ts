774cd8dd9c82b96d8bfe82e22eda1391029a7b2c
ahmedmuab <ahmed@muab.info>
2025-04-24 16:11:54 +0500

    Create environment
    Create environment

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,6 @@
 export const env = {
   // local: { API_URL: "http://localhost:4000/api/admin" },
-  dev: { API_URL: "https://api-dev.muab.info/api/admin" },
+  dev: { API_URL: process.env.NEXT_PUBLIC_API_URL },
   // prod: { API_URL: "" },
 };
 
